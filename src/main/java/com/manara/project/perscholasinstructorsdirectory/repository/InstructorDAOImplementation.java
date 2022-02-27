07111e1a54bd7d24e43f0aeb5968bd86d9f2204e
Manara Ali <manaraali64@gmail.com>
2022-02-27 15:37:30 -0500

    Create the InstructorDAO implementation

---
+++ src/main/java/com/manara/project/perscholasinstructorsdirectory/repository/InstructorDAOImplementation.java
@@ -0,0 +1,42 @@
+package com.manara.project.perscholasinstructorsdirectory.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.manara.project.perscholasinstructorsdirectory.model.Instructor;
+
+@Repository
+public class InstructorDAOImplementation implements InstructorDAO {
+
+	// Create a field to receive the EntiyManager injection
+	private EntityManager entityManager;
+	
+	// Use constructor injection
+	@Autowired
+	public InstructorDAOImplementation(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	@Override
+	@Transactional
+	public List<Instructor> findAll() {
+		// 1. Retrieve the current session
+		Session session = entityManager.unwrap(Session.class);
+		
+		// 2. Create a query to retrieve all the instructors
+		Query<Instructor> query = session.createQuery("FROM Instructor", Instructor.class);
+		
+		// 3. Create a list of all instructor
+		List<Instructor> instructorList = query.getResultList();
+		
+		// 4. Return the list of instructor
+		return instructorList;
+	}
+
+}
