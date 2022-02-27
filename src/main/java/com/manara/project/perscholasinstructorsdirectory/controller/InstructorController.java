292fab3824f80e00aecb974dee6a4576b4c7c95b
Manara Ali <manaraali64@gmail.com>
2022-02-27 17:33:30 -0500

    Add more routes for CRUD method

--- src/main/java/com/manara/project/perscholasinstructorsdirectory/controller/InstructorController.java
+++ src/main/java/com/manara/project/perscholasinstructorsdirectory/controller/InstructorController.java
@@ -3,7 +3,12 @@ package com.manara.project.perscholasinstructorsdirectory.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +32,43 @@ public class InstructorController {
 	@GetMapping("/instructors")
 	public List<Instructor> getAllInstructors() {
 		return instructorDAO.findAll();
+	}
+	
+	@GetMapping("/instructors/{id}")
+	public Instructor getInstructor(@PathVariable int id) {
+		// Find instructor
+		Instructor instructor = instructorDAO.findById(id);
+		
+		// Assuming no instructor is found
+		if(instructor == null) throw new RuntimeException("No instructor found with id: " + id);
+		
+		return instructor;
+	}
+	
+	@PostMapping("/instructors/add")
+	public Instructor addInstructor(@RequestBody Instructor instructor) {
+		System.out.println(instructor);
+		
+		instructor.setId(0);
+		
+		Instructor newInstructor = instructorDAO.save(instructor);
+		
+		return newInstructor;
+	}
+	
+	@PutMapping("/instructors/update")
+	public Instructor updateInstructor(@RequestBody Instructor instructor) {
+		return instructorDAO.save(instructor);
+	}
+	
+	@DeleteMapping("/instructors/delete/{id}")
+	public String deleteInstructor(@PathVariable int id) {
+		Instructor instructor = instructorDAO.findById(id);
+		
+		if(instructor == null) return "No instructor found with id of: " + id;
+		
+		instructorDAO.delete(id);
+		
+		return "Instructor " + instructor.getFirstName() + ", " + instructor.getFirstName() + " deleted!";
 	}
 }
