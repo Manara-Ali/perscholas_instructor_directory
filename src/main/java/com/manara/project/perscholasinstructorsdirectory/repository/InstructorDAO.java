258bb783e3532e29296d38cc1c34edddd52b790b
Manara Ali <manaraali64@gmail.com>
2022-02-27 17:32:21 -0500

    Create additional CRUD method for DAO

--- src/main/java/com/manara/project/perscholasinstructorsdirectory/repository/InstructorDAO.java
+++ src/main/java/com/manara/project/perscholasinstructorsdirectory/repository/InstructorDAO.java
@@ -6,5 +6,15 @@ import com.manara.project.perscholasinstructorsdirectory.model.Instructor;
 
 public interface InstructorDAO {
 
+	// Create a method to retrieve all instructor
 	public List<Instructor> findAll();
+	
+	// Create a method to retrieve a single instructor
+	public Instructor findById(int id);
+	
+	// Create a method to add a new instructor
+	public Instructor save(Instructor instructor);
+	
+	// Create a method to delete instructor
+	public void delete(int id);
 }
