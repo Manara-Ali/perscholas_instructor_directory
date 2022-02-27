fbef179c6969c6c5ef21deb77640ecc6ba275412
Manara Ali <manaraali64@gmail.com>
2022-02-27 13:58:33 -0500

    Create new maven project

---
+++ src/main/java/com/manara/project/perscholasinstructorsdirectory/PerscholasinstructorsdirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.manara.project.perscholasinstructorsdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerscholasinstructorsdirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerscholasinstructorsdirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manara/project/perscholasinstructorsdirectory/PerscholasinstructorsdirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manara.project.perscholasinstructorsdirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerscholasinstructorsdirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
