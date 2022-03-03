5a4ea0ebeb984542a0da35c115682248c918cdd7
Manara Ali <manaraali64@gmail.com>
2022-03-03 15:22:26 -0500

    Create a Review Service class

---
+++ src/main/java/com/manara/project/perscholasinstructorsdirectory/service/ReviewService.java
@@ -0,0 +1,24 @@
+package com.manara.project.perscholasinstructorsdirectory.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.manara.project.perscholasinstructorsdirectory.model.Course;
+import com.manara.project.perscholasinstructorsdirectory.model.Review;
+
+@Service
+public interface ReviewService {
+
+	// 1. Create a method to get all the reviews
+	public List<Review> findAll();
+	
+	// 2. Create a method to add a review
+	public Review save(Review review);
+	
+	// 3. Create a method to retrieve a single review
+	public Review findById(int id);
+	
+	// 4. Create a method to save review to course
+	public Review save(int id, Review review);
+}
