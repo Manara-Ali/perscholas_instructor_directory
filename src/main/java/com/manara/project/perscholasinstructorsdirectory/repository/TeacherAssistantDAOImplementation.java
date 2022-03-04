495e0041feabbea0e5ffdfa938e249fd016d8d0f
Manara Ali <manaraali64@gmail.com>
2022-03-04 15:15:14 -0500

    Create Teacher Assistant DAO Implementation

---
+++ src/main/java/com/manara/project/perscholasinstructorsdirectory/repository/TeacherAssistantDAOImplementation.java
@@ -0,0 +1,55 @@
+package com.manara.project.perscholasinstructorsdirectory.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.manara.project.perscholasinstructorsdirectory.model.TeacherAssistant;
+
+@Repository
+public class TeacherAssistantDAOImplementation implements TeacherAssistantDAO {
+
+	// Create a field to use as our Session Factory
+	private EntityManager entityManager;
+	
+	// Use constructor injection to inject a Session Factory into our field
+	@Autowired
+	public TeacherAssistantDAOImplementation(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	@Override
+	public List<TeacherAssistant> findAll() {
+		// 1. Retrieve the current session
+		Session session = entityManager.unwrap(Session.class);
+		
+		// 2. Create a query for all TA
+		Query<TeacherAssistant> query = session.createQuery("FROM TeacherAssistant");
+		
+		// 3. Create a list of all TA
+		List<TeacherAssistant> teacherAssistantList = query.getResultList();
+		
+		// 4. Return the list of TA
+		return teacherAssistantList;
+	}
+
+	@Override
+	public TeacherAssistant save(TeacherAssistant teacherAssistant) {
+		// 1. Retrieve the current session
+		Session session = entityManager.unwrap(Session.class);
+		
+		// 2. Force a save on new TA
+		teacherAssistant.setId(0);
+		
+		// 3. Save the TA
+		session.saveOrUpdate(teacherAssistant);
+		
+		return teacherAssistant;
+	}
+
+}
