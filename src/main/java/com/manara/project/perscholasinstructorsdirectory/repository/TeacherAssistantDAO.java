4e48ac45b1f8905f49f787e9ae0799a51fbb5bca
Manara Ali <manaraali64@gmail.com>
2022-03-05 16:32:47 -0500

    Create Teacher Assistant DAO

--- src/main/java/com/manara/project/perscholasinstructorsdirectory/repository/TeacherAssistantDAO.java
+++ src/main/java/com/manara/project/perscholasinstructorsdirectory/repository/TeacherAssistantDAO.java
@@ -2,6 +2,7 @@ package com.manara.project.perscholasinstructorsdirectory.repository;
 
 import java.util.List;
 
+import com.manara.project.perscholasinstructorsdirectory.model.Instructor;
 import com.manara.project.perscholasinstructorsdirectory.model.TeacherAssistant;
 
 public interface TeacherAssistantDAO {
@@ -9,6 +10,15 @@ public interface TeacherAssistantDAO {
 	// Create a method to retrieve all the teacher assistants
 	public List<TeacherAssistant> findAll();
 	
-	// Create a method to save/update teacher assistant
+	// Create a method to save/update teacher assistant alone
 	public TeacherAssistant save(TeacherAssistant teacherAssistant);
+	
+	// Create a method to find a single teacher assistant by its id
+	public TeacherAssistant findById(int id);
+	
+	// Create a method to delete a TA
+	public void deleteById(int id);
+	
+	// Create a method to update TA information and instructor
+	public TeacherAssistant save(TeacherAssistant teacherAssistant, int instructorId);
 }
