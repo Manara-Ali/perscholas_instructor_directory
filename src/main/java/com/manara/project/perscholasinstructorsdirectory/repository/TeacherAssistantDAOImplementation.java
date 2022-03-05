fea842b98cd7368bc9e8601532c46d1b3107311e
Manara Ali <manaraali64@gmail.com>
2022-03-05 16:33:22 -0500

    Create Teacher Assistant DAO Implementation

--- src/main/java/com/manara/project/perscholasinstructorsdirectory/repository/TeacherAssistantDAOImplementation.java
+++ src/main/java/com/manara/project/perscholasinstructorsdirectory/repository/TeacherAssistantDAOImplementation.java
@@ -9,6 +9,7 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.manara.project.perscholasinstructorsdirectory.model.Instructor;
 import com.manara.project.perscholasinstructorsdirectory.model.TeacherAssistant;
 
 @Repository
@@ -42,11 +43,46 @@ public class TeacherAssistantDAOImplementation implements TeacherAssistantDAO {
 	public TeacherAssistant save(TeacherAssistant teacherAssistant) {
 		// 1. Retrieve the current session
 		Session session = entityManager.unwrap(Session.class);
-		
+
 		// 2. Force a save on new TA
-		teacherAssistant.setId(0);
-		
+//		teacherAssistant.setId(0);
+						
 		// 3. Save the TA
+		session.saveOrUpdate(teacherAssistant);
+		
+		return teacherAssistant;
+	}
+
+
+	@Override
+	public TeacherAssistant findById(int id) {
+		// 1. Retrieve the current session
+		Session session = entityManager.unwrap(Session.class);
+		
+		// 2. Find teacher assistant
+		TeacherAssistant teacherAssistant = session.get(TeacherAssistant.class, id);
+		
+		return teacherAssistant;
+	}
+
+	@Override
+	public void deleteById(int id) {
+		// 1. Retrieve the current session
+		Session session = entityManager.unwrap(Session.class);
+		
+		// 2. Find the TA
+		TeacherAssistant teacherAssistant = session.get(TeacherAssistant.class, id);
+		
+		// 3. Delete TA
+		session.delete(teacherAssistant);
+	}
+
+	@Override
+	public TeacherAssistant save(TeacherAssistant teacherAssistant, int instructorId) {
+		// 1. Retrieve the current session
+		Session session = entityManager.unwrap(Session.class);
+		
+		// 2. Save TA
 		session.saveOrUpdate(teacherAssistant);
 		
 		return teacherAssistant;
