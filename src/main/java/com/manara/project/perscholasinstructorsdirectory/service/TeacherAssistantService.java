4d2e2dbdd19709471657c04ee659ab02f2fa3bec
Manara Ali <manaraali64@gmail.com>
2022-03-05 16:34:32 -0500

    Create Teacher Assistant Service class

--- src/main/java/com/manara/project/perscholasinstructorsdirectory/service/TeacherAssistantService.java
+++ src/main/java/com/manara/project/perscholasinstructorsdirectory/service/TeacherAssistantService.java
@@ -2,6 +2,7 @@ package com.manara.project.perscholasinstructorsdirectory.service;
 
 import java.util.List;
 
+import com.manara.project.perscholasinstructorsdirectory.model.Instructor;
 import com.manara.project.perscholasinstructorsdirectory.model.TeacherAssistant;
 
 public interface TeacherAssistantService {
@@ -9,6 +10,15 @@ public interface TeacherAssistantService {
 	// Create a method to list all TA
 	public List<TeacherAssistant> findAll();
 	
-	// Create a method to save/update a new TA
+	// Create a method to save/update a new TA alone
 	public TeacherAssistant save(TeacherAssistant teacherAssistant);
+	
+	// Create a method to save/update TA with instructor
+	public TeacherAssistant save(TeacherAssistant teacherAssistant, int instructorId);
+	
+	// Create a method to find a single teacher assistant by its id
+	public TeacherAssistant findById(int id);
+	
+	// Create a method to delete a TA
+	public void deleteById(int id);
 }
